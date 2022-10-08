acb795cc9c1a90daaa123dfbb560ee297b3283f0
AminAbA <amin.abbaschian@gmail.com>
2022-10-08 19:15:28 +0330

    CreateFirstTimeRepos

---
+++ Esm.Core/Senders/SendEmail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace TopLearn.Core.Senders
+{
+    public class SendEmail
+    {
+        public static void Send(string to,string subject,string body)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("");
+            mail.From = new MailAddress("", "");
+            mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+            SmtpServer.Port = 25;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("Email", "Pass");
+
+            SmtpServer.Send(mail);
+
+        }
+    }
+}
---
+++ Esm/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Esm.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Register
+
+        #endregion
+
+        #region Login
+
+        #endregion
+
+        #region ActivateAccount
+
+        #endregion
+
+        #region LogOut
+
+        #endregion
+
+        #region ForgotPassword
+
+        #endregion
+
+        #region ResetPassword
+
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
